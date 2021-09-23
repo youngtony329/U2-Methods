3d5c7bdc2c770ac94ef179a98f9b5a9fe6b1dea4
Tony Young <youngton000@warren.k12.in.us>
2021-09-23 11:42:02 -0400

    New Stuff

--- L4-Strings/src/LastLetterWord.java
+++ L4-Strings/src/LastLetterWord.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class LastLetterWord {
 
     /*
@@ -9,10 +11,28 @@ public class LastLetterWord {
     Ex:  Goodbye, Hello, Morning -> byelloing
     */
 
+    /*
+    1. Get Input: three words
+    2. Isolate the last three letters of any word
+    3. Combine #2 of all three words
+     */
+
     public static void main(String[] args) {
 
+        String word1 = JOptionPane.showInputDialog("Enter Word 1: ");
+        String word2 = JOptionPane.showInputDialog("Enter Word 2: ");
+        String word3 = JOptionPane.showInputDialog("Enter Word 3: ");
 
+        String output = lastThree(word1) + lastThree(word2) + lastThree(word3);
 
+        JOptionPane.showMessageDialog(null, output);
+
+        System.exit(0);
+
+    }
+
+    public static String lastThree(String word){
+          return word.substring(word.length()-3);
     }
 
 }
